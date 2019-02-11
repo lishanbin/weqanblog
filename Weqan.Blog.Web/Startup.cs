eb105f5c594bb62f70c6492627775b742e5073aa
lishanbin <984901912@qq.com>
2019-02-11 11:04:55 +0800

    后台增删改查

--- Weqan.Blog.Web/Startup.cs
+++ Weqan.Blog.Web/Startup.cs
@@ -55,6 +55,10 @@ namespace Weqan.Blog.Web
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "admin",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                  );
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
