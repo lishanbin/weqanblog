9e0a01910cd331113e505bb3735676bc881c0e40
lishanbin <984901912@qq.com>
2019-02-18 15:19:19 +0800

    基本开发完成

--- Weqan.Blog.DAL/CategoryDAL.cs
+++ Weqan.Blog.DAL/CategoryDAL.cs
@@ -12,6 +12,45 @@ namespace Weqan.Blog.DAL
     public class CategoryDAL
     {
         /// <summary>
+        /// 编号计算
+        /// </summary>
+        /// <param name="pbh">父编号</param>
+        /// <param name="x">每一级编号的位数</param>
+        /// <returns></returns>
+        public string GenBH(string pbh,int x)
+        {
+            string sql = "select right(max(bh)," + x + ") from Category where pbh=" + pbh;
+            using (var connection=ConnectionFactory.GetOpenConnection())
+            {
+                string res = connection.ExecuteScalar<string>(sql);
+                if (string.IsNullOrEmpty(res))
+                {
+                    int a = 1;
+                    if (pbh!="0")
+                    {
+                        return pbh + a.ToString("d" + x);
+                    }
+                    return a.ToString("d" + x);
+                }
+                else
+                {
+                    int a = int.Parse(res) + 1;
+                    int b = (int)Math.Pow(10, x);
+                    if (a>=b)
+                    {
+                        //return "编号超过限制！";
+                        throw new Exception("编号超过限制！");
+                    }
+                    if (pbh!="0")
+                    {
+                        return pbh + a.ToString("d" + x);
+                    }
+                    return a.ToString("d" + x);
+                }
+            }
+        }
+
+        /// <summary>
         /// 新增
         /// </summary>
         /// <param name="m"></param>
@@ -35,6 +74,24 @@ namespace Weqan.Blog.DAL
             {
                 var m = connection.Query<Model.Category>("select * from Category where Bh=@Bh", new { Bh = caBh }).FirstOrDefault();
                 return m;
+            }
+        }
+        /// <summary>
+        /// 计算记录数
+        /// </summary>
+        /// <param name="cond"></param>
+        /// <returns></returns>
+        public int CalcCount(string cond)
+        {
+            string sql = "select count(1) from Category";
+            if (!string.IsNullOrEmpty(cond))
+            {
+                sql += $" where {cond}";
+            }
+            using (var connection=ConnectionFactory.GetOpenConnection())
+            {
+                int res = connection.ExecuteScalar<int>(sql);
+                return res;
             }
         }
 
@@ -109,10 +166,29 @@ namespace Weqan.Blog.DAL
                 return false;
             }
         }
-
-
-
-
-
+        /// <summary>
+        /// 取所有分类的数据拼接成json字符中返回
+        /// </summary>
+        /// <returns></returns>
+        public dynamic GettreeJson()
+        {
+            List<Model.TreeNode_LayUI> list_return = new List<TreeNode_LayUI>();
+            List<Model.Category> list = GetList("");
+            var top = list.Where(a => a.Pbh == "0");
+            foreach (var item in top)
+            {
+                Model.TreeNode_LayUI node = new TreeNode_LayUI() { id = item.Id, name = item.CaName, spread = true ,pid=0};
+                var sub = list.Where(a => a.Pbh == item.Bh);
+                List<Model.TreeNode_LayUI> list_sub = new List<TreeNode_LayUI>();
+                foreach (var item2 in sub)
+                {
+                    Model.TreeNode_LayUI node2 = new TreeNode_LayUI() { id = item2.Id, name = item2.CaName, spread = true,pid=item.Id };
+                    list_sub.Add(node2);
+                }
+                node.children = list_sub;
+                list_return.Add(node);
+            }
+            return Newtonsoft.Json.JsonConvert.SerializeObject(list_return);
+        }
     }
 }
---
+++ Weqan.Blog.Model/TreeNode_LayUI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weqan.Blog.Model
+{
+    /// <summary>
+    /// layui的树形菜单的节点模型
+    /// </summary>
+    public class TreeNode_LayUI
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public bool spread { get; set; }
+        public List<TreeNode_LayUI> children { get; set; }
+        public int pid { get; set; }
+    }
+}
